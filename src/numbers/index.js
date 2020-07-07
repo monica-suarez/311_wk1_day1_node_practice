f2132321f8e9cc6e074e88e41775df54f99bf519
monica-suarez <shutterbugmo@gmail.com>
2020-07-07 14:33:08 -0500

    tests pass

--- src/numbers/index.js
+++ src/numbers/index.js
@@ -1,16 +1,30 @@
 const isEven = (num) => {
   // write code for numbers.isEven
-
+  if(num % 2 === 0){
+    return true;
+  }else{
+    return false;
+  }
 }
 
 const sum = (arr) => {
   // write code for numbers.sum
-
+  let arrSum = arr.reduce((a,b) => a + b, 0)
+  return arrSum;
 }
 
 const comboSum = (arr, sum) => {
   // write code for numbers.comboSum
-
+  for (let i = 0; i < arr.length; i++){
+    for (let j = 0; j < arr.length; j++){
+      if (arr[i] + arr[j] === sum) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  }
+  
 }
 
 module.exports = {
--- src/strings/index.js
+++ src/strings/index.js
@@ -1,16 +1,20 @@
 const split = (str, delim) => {
   // write code for strings.split
-
+  return str.split(delim)
 }
 
 const pairs = (str) => {
   // write code for strings.pairs
+  return str.match(/(..?)/g)
 
 }
 
 const reverse = (str) => {
   // write code for strings.reverse
-
+  splitIt = str.split('')
+  reverseIt = splitIt.reverse()
+  reverseWord = reverseIt.join('')
+  return reverseWord;
 }
 
 module.exports = {
